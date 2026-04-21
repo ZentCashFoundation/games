6403fa0a2bbfb9843aee143e0f0db85ac5cf99d5
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2026-04-22 00:42:59 +0200

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,27 @@
+if (!token) {
+  document.querySelector('.userdashboard').style.display = 'none';
+  document.querySelector('.usergetbalance').style.display = 'none';
+  document.querySelector('.close-session-btn').style.display = 'none';
+}
+
+
+/** Fetch game costs and display them in the UI */
+getGameCosts()  
+
+/** Session management */
+  async function closeSession() {
+    localStorage.removeItem('token');
+    location.reload();
+}
+/** End of session management */
+
+/** Set background music volume */
+const audio = document.getElementById("background-music");
+audio.volume = 0.08;
+audio.play();
+
+/** Copyright Automatic Year */
 const getCurrentYear = ()=>{
     return new Date().getFullYear();
 }
 document.getElementById('currentyear').textContent = getCurrentYear()
-
-// =======================
-// Configuración de API
-// =======================
-const API = "https://api.games.zent.cash:3000/api";
-var token = localStorage.getItem("token") || null;
-let gameSessionId = null;
-
-async function getGameCosts() {
-  try {
-    const response = await fetch(`${API}/game/list`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const data = await response.json();
-
-    data.games.forEach(game => {
-      const id = `${game.name}-cost`;
-      const el = document.getElementById(id);
-
-      if (el) {
-        el.textContent = game.cost;
-      } else {
-        console.warn(`Elemento no encontrado: ${id}`);
-      }
-    });
-
-  } catch (error) {
-    console.error('Error fetching game costs:', error);
-  }
-}
-getGameCosts()
-
-  const audio = document.getElementById("background-music");
-  audio.volume = 0.08;
