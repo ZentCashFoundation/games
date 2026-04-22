d922cec7f766174eec28102ee8a6109ab0ed8a82
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2026-04-22 12:10:38 +0200

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,44 @@
-if (!token) {
-  document.querySelector('.userdashboard').style.display = 'none';
-  document.querySelector('.usergetbalance').style.display = 'none';
-  document.querySelector('.close-session-btn').style.display = 'none';
-}
+document.addEventListener("DOMContentLoaded", async() => {
+    
+    const emailInput = document.getElementById('email');
 
+    if (emailInput && emailInput.value.trim() === '') {
+        const savedEmail = localStorage.getItem("email");
+        if (savedEmail) {
+            emailInput.value = savedEmail;
+        }
+    }
+    
+    const userdashboard = document.querySelector('.userdashboard');
+    const usergetbalance = document.querySelector('.usergetbalance');
+    const registerloginbtn = document.querySelector('.register-login-btn');
+    const closesessionbtn = document.querySelector('.close-session-btn');
+
+    const authuser = document.querySelector('.auth-container');
+    
+    if (!token) {
+        userdashboard.style.display = "none";
+        usergetbalance.style.display = "none";
+        registerloginbtn.style.display = "initial";
+		    closesessionbtn.style.display = "none";
+    } else {
+        if (authuser) {
+            authuser.style.display = "none";
+        }
+        usergetbalance.style.display = "initial";
+        registerloginbtn.style.display = "none";  
+		    closesessionbtn.style.display = "initial";
+        
+        getBalance().then(() => {
+            const balance = document.getElementById('balance');
+            const userbalance = document.getElementById('getbalance');
+
+            if (balance && userbalance) {
+                userbalance.textContent = balance.textContent;
+            }
+        });
+      }  
+    });
 
 /** Fetch game costs and display them in the UI */
 getGameCosts()  
@@ -11,7 +46,7 @@ getGameCosts()
 /** Session management */
   async function closeSession() {
     localStorage.removeItem('token');
-    location.reload();
+    location.href = "./";
 }
 /** End of session management */
 
